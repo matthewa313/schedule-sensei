269a72f288272a11d91051890cde26ab24fcfddb
Matthew Anderson <mwanderson04@gmail.com>
2022-02-20 22:43:20 -0700

    Build structure and initial brand of app
    - Initialize branding with names (still need visual brand)
    - Install mui (material ui) including relevant stylesheets
    - Use mui to build simple app bar and theme
    - Extract metadata (website name etc) into build-time variables

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { AppBar,
+  Typography,
+  Toolbar,
+  ThemeProvider,
+  Container
+} from '@mui/material';
+
+import { createTheme } from '@mui/material/styles';
+import { red, blue } from '@mui/material/colors';
+
+const creekTheme = createTheme({
+  palette: {
+    primary: red,
+    secondary: blue,
+  }
+})
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={creekTheme}>
+      <div className="App">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h5">
+              Schedule Sensei
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="sm">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Container>
+      </div>
+    </ThemeProvider>
   );
 }
 
