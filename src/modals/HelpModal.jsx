3228241891300ea82a852693befea946c9cbe4c6
Matthew Anderson <mwanderson04@gmail.com>
2022-02-22 19:58:26 -0700

    Build Help Modal with Bear Sensei brand

--- src/modals/HelpModal.jsx
+++ src/modals/HelpModal.jsx
@@ -9,14 +9,53 @@ import {
   DialogTitle,
 } from '@mui/material';
 
+import { makeStyles } from '@mui/styles';
+import bearSensei from '../sensei-art/bearsensei.jpg';
+
+
+const useStyles = makeStyles((creekTheme) => ({
+  title: {
+    textAlign: 'center',
+  },
+  leftColumn: {
+    float: 'left',
+    width: '60%',
+    height: '100%',
+    padding: '1em',
+    textAlign: 'justify',
+  },
+  rightColumn: {
+    float: 'right',
+    width: '40%',
+    height: '100%',
+    padding: '1em',
+    textAlign: 'justify',
+  },
+}));
+
 const HelpModal = ({isOpen, handleClose}) => {
+  const classes = useStyles();
+
   return (
     <Dialog open={isOpen} onClose={handleClose}>
-      <DialogTitle>About</DialogTitle>
+      <DialogTitle className={classes.title}>Welcome to the Schedule Sensei Dojo!</DialogTitle>
       <DialogContent>
-        <DialogContentText>
-          Help! Helpity help helpity! Help me, help him, help her, and especially help the needy! Help. Yelp. Kelp. Telp. Doesnt matter, just elp! Elp Elp Elpity!
-        </DialogContentText>
+        <div style={{display: 'flex'}}>
+          <div className={classes.leftColumn} >
+            <DialogContentText>
+              Creek Students,<br/><br/>
+
+              Making your own schedule can be hard because it is difficult to know what schedules you can get. On top of that, you want certain off periods and your favorite teachers!<br/><br/>
+
+              This tool is here to help. Use this website to generate a list of all your possible schedules, and filter by your preferred off periods or your favorite teachers! Then save the list to help during your self scheduling session.<br/><br/>
+
+              For the best experience, use a computer.
+            </DialogContentText>
+          </div>
+          <div className={classes.rightColumn}>
+            <img src={bearSensei} alt='bear sensei'/>
+          </div>
+        </div>
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose} color='primary'>
